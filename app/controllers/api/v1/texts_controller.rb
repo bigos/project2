21022be54197e7a1f73bb5a9cc38cd584a87ae9b
Jacek Podkanski <ruby.object@googlemail.com>
2023-05-07 00:08:41 +0100

    corrected error in routes and prepared two simple Texts actions

--- app/controllers/api/v1/texts_controller.rb
+++ app/controllers/api/v1/texts_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::TextsController < ApplicationController
+
+  # GET /api/v1/texts(.:format)
+  api :GET, '/api/v1/texts(.:format)'
+  def index
+    @texts = Text.all
+    render json: @texts
+  end
+
+  # GET /api/v1/texts/:id(.:format)
+  api :GET, '/api/v1/texts/:id(.:format)'
+  param :id, :number, desc: 'id of the requested Text'
+  def show
+    @text = Text.find(params[:id])
+    render json: @text
+  end
 end

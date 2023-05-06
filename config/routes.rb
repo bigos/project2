56866533731980c4e910fc8d5086e8955db4adda
Jacek Podkanski <ruby.object@googlemail.com>
2023-05-06 23:34:13 +0100

    now we have routes that work with our controllers
    controllers were generated based on example text found here
    https://medium.com/@oliver.seq/creating-a-rest-api-with-rails-2a07f548e5dc
    
    rails g controller api/v1/Words
    rails g controller api/v1/Texts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :vi do
+      resources :words do
+      end
+
+      resources :texts do
+
+      end
+    end
+
+  end
+
   apipie
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

3ee921092735326889588053a0bffc45edf2df5b
Jacek Podkanski <ruby.object@googlemail.com>
2023-05-07 00:27:17 +0100

    another go ad apipie

--- app/controllers/api/v1/texts_controller.rb
+++ app/controllers/api/v1/texts_controller.rb
@@ -1,14 +1,16 @@
 class Api::V1::TextsController < ApplicationController
 
+  # https://iliabylich.github.io/2015/06/07/apipie-amazing-tool-for-documenting-your-rails-api.html
+
   # GET /api/v1/texts(.:format)
-  api :GET, '/api/v1/texts(.:format)'
+  api! 'List of Texts'
   def index
     @texts = Text.all
     render json: @texts
   end
 
   # GET /api/v1/texts/:id(.:format)
-  api :GET, '/api/v1/texts/:id(.:format)'
+  api! 'Show Text'
   param :id, :number, desc: 'id of the requested Text'
   def show
     @text = Text.find(params[:id])

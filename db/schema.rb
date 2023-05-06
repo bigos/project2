02cb8abedbaa15b3d06c3bfeee53cdebe2654d26
Jacek Podkanski <ruby.object@googlemail.com>
2023-05-06 23:27:38 +0100

    ran 3 migrations

---
+++ db/schema.rb
@@ -0,0 +1,40 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2023_05_06_222605) do
+  create_table "texts", force: :cascade do |t|
+    t.string "title"
+    t.string "link"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "usages", force: :cascade do |t|
+    t.integer "word_id", null: false
+    t.integer "text_id", null: false
+    t.integer "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["text_id"], name: "index_usages_on_text_id"
+    t.index ["word_id"], name: "index_usages_on_word_id"
+  end
+
+  create_table "words", force: :cascade do |t|
+    t.string "word"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "usages", "texts"
+  add_foreign_key "usages", "words"
+end
